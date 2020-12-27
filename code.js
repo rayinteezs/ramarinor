015fe01b757b4d73b9abac05fc566f9111e87edb
rayinteezs <a1104512583@sina.com>
2020-12-28 04:59:47 +0800

    added let where it was missing

--- code.js
+++ code.js
@@ -7,11 +7,11 @@ let bigMacPriceBritain = 3.49;
 let bigMacPriceBrazil = 22.9;
 
 //Calculating the BigMac index
-bigMacIndexVenezuela = bigMacPriceVenezuela / bigMacPriceUSA;
-bigMacIndexSwitzerland = bigMacPriceSwitzerland / bigMacPriceUSA;
-bigMacIndexCanada = bigMacPriceCanada / bigMacPriceUSA;
-bigMacIndexBritain = bigMacPriceBritain / bigMacPriceUSA;
-bigMacIndexBrazil = bigMacPriceBrazil / bigMacPriceUSA;
+let bigMacIndexVenezuela = bigMacPriceVenezuela / bigMacPriceUSA;
+let bigMacIndexSwitzerland = bigMacPriceSwitzerland / bigMacPriceUSA;
+let bigMacIndexCanada = bigMacPriceCanada / bigMacPriceUSA;
+let bigMacIndexBritain = bigMacPriceBritain / bigMacPriceUSA;
+let bigMacIndexBrazil = bigMacPriceBrazil / bigMacPriceUSA;
 
 //Logging the results in the console
 console.log("Venezuela's Big Mac Index is " + bigMacIndexVenezuela.toFixed(2));
@@ -23,18 +23,18 @@ console.log("Britain's Big Mac Index is " + bigMacIndexBritain.toFixed(2));
 console.log("Brazil's Big Mac Index is " + bigMacIndexBrazil.toFixed(2));
 
 //Assigning the actual exchange rates
-usdToVenezuelanBolivar = 3613989.07;
-usdToSwissFranc = 0.92;
-usdToCanadianDollar = 1.27;
-usdToBritishPound = 0.73;
-usdToBrazilianReal = 5.25;
+let usdToVenezuelanBolivar = 3613989.07;
+let usdToSwissFranc = 0.92;
+let usdToCanadianDollar = 1.27;
+let usdToBritishPound = 0.73;
+let usdToBrazilianReal = 5.25;
 
 //Calculating the difference of the Big Mac index with the actual exchange rate
-venezuelaDifferenceInPercentage = (bigMacIndexVenezuela - usdToVenezuelanBolivar) * 100 /usdToVenezuelanBolivar;
-switzerlandDifferenceInPercentage = (bigMacIndexSwitzerland - usdToSwissFranc) * 100 /usdToSwissFranc;
-canadaDifferenceInPercentage = (bigMacIndexCanada- usdToCanadianDollar) * 100 /usdToCanadianDollar;
-britainDifferenceInPercentage = (bigMacIndexBritain - usdToBritishPound) * 100 /usdToBritishPound;
-brazilDifferenceInPercentage = (bigMacIndexBrazil - usdToBrazilianReal) * 100 /usdToBrazilianReal;
+let venezuelaDifferenceInPercentage = (bigMacIndexVenezuela - usdToVenezuelanBolivar) * 100 /usdToVenezuelanBolivar;
+let switzerlandDifferenceInPercentage = (bigMacIndexSwitzerland - usdToSwissFranc) * 100 /usdToSwissFranc;
+let canadaDifferenceInPercentage = (bigMacIndexCanada- usdToCanadianDollar) * 100 /usdToCanadianDollar;
+let britainDifferenceInPercentage = (bigMacIndexBritain - usdToBritishPound) * 100 /usdToBritishPound;
+let brazilDifferenceInPercentage = (bigMacIndexBrazil - usdToBrazilianReal) * 100 /usdToBrazilianReal;
 
 //Logging the difference in the console:
 console.log('The difference between the Big Mac index and the actual exchange rate in Venezela is: ' + venezuelaDifferenceInPercentage.toFixed(2) + "%");
